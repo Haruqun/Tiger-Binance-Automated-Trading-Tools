11ce2df145f47f50471f45467d87469678ee98d1
Haruqun <info@fivenine-design.com>
2023-05-01 00:06:21 +0900

    安定版

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Tiger-Binance-Automated-Trading-Tools
-We are not responsible for any results obtained from the use of this tool. You need to set up your Binance API key.
+# Binance Automated Trading Bot
+
+## Overview
+このプログラムは、Binanceの仮想通貨取引所で自動取引を行うためのボットです。ボットは、移動平均線とボリンジャーバンドを使用して売買シグナルを生成し、条件に応じて買い注文または売り注文を出します。また、注文時にはストップロスとテイクプロフィットの設定も行います。
+
+## Features
+- 移動平均線に基づく売買シグナルの生成
+- ボリンジャーバンドに基づく売買シグナルの生成
+- ストップロスとテイクプロフィットの設定
+- リスク管理に基づく注文の制限
+- 取引チャートの描画
+
+## How to Use
+1. BinanceのAPIキーとシークレットキーを取得してください。
+2. `.env`ファイルを作成し、以下の形式でAPIキーとシークレットキーを記入してください。
+BINANCE_API_KEY=YOUR_API_KEY
+BINANCE_SECRET_KEY=YOUR_SECRET_KEY
+
+markdown
+Copy code
+3. 必要に応じて、プログラム内の以下の変数を変更してください。
+symbol = "BNBUSDT" # 取引対象のペア
+quantity = 0.1 # 取引量
+short_period = 20 # 短期移動平均線の期間
+long_period = 50 # 長期移動平均線の期間
+stop_loss_pct = 0.01 # ストップロスの割合
+take_profit_pct = 0.02 # テイクプロフィットの割合
+risk_pct = 0.01 # リスク管理の割合
+
+markdown
+Copy code
+4. プログラムを実行して、自動取引を開始してください。
+python main.py
+
+注: このプログラムはデモンストレーション用であり、実際の取引には十分なテストと検証が必要です。リスクを理解し、自己責任でご使用ください。
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import numpy as np
 import time
 from binance.exceptions import BinanceAPIException
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # APIキーを読み込む
 load_dotenv()
@@ -15,62 +17,64 @@ client = Client(API_KEY, API_SECRET)
 
 # 取引対象の仮想通貨
 symbol = "BNBUSDT"
-
-# 取引量
-quantity = 0.001
-
-# 移動平均線の期間
+base_asset = "BNB"
+quote_asset = "USDT"
+quantity = 0.1
 short_period = 20
 long_period = 50
+stop_loss_pct = 0.01
+take_profit_pct = 0.02
+risk_pct = 0.01
 
 
 def get_price_data():
-    # 過去の価格データを取得する
+    # 過去の価格データを取得する（過去50時間分のデータ）
     klines = client.get_historical_klines(
-        symbol, Client.KLINE_INTERVAL_1HOUR, "24 hour ago UTC"
+        symbol, Client.KLINE_INTERVAL_1HOUR, "50 hours ago UTC"
     )
-
-    # ローソク足の終値のみを取り出す
     close_prices = np.array([float(kline[4]) for kline in klines])
-
-    # 現在の価格を取得する
     ticker = client.get_symbol_ticker(symbol=symbol)
     current_price = float(ticker["price"])
-
     return close_prices, current_price
 
 
 def calc_moving_average(close_prices):
-    # 移動平均線を計算する
     short_ma = np.mean(close_prices[-short_period:])
     long_ma = np.mean(close_prices[-long_period:])
-
     return short_ma, long_ma
 
 
-def place_order(side, quantity, symbol, price=None):
+def calc_bollinger_bands(close_prices, window=20, multiplier=2):
+    sma = np.mean(close_prices[-window:])
+    std = np.std(close_prices[-window:])
+    upper_band = sma + (multiplier * std)
+    lower_band = sma - (multiplier * std)
+    return sma, upper_band, lower_band
+
+
+def place_order(side, quantity, symbol, stop_loss_pct, take_profit_pct):
     try:
-        # Check if there's enough balance
-        base_asset = symbol[:-4]  # Extract the base asset, e.g., BTC from BTCUSDT
-        balance = client.get_asset_balance(asset=base_asset)
-        available_balance = float(balance["free"])
-
-        if side == "sell" and available_balance < quantity:
-            print(f"Insufficient balance to place {side} order of {quantity} {symbol}.")
-            return None
-        if not price:
-            ticker = client.get_symbol_ticker(symbol=symbol)
-            current_price = float(ticker["price"])
+        ticker = client.get_symbol_ticker(symbol=symbol)
+        current_price = float(ticker["price"])
+        if side == "buy":
+            stop_loss = current_price * (1 - stop_loss_pct)
+            take_profit = current_price * (1 + take_profit_pct)
         else:
-            current_price = price
-
+            stop_loss = current_price * (1 + stop_loss_pct)
+            take_profit = current_price * (1 - take_profit_pct)
+        if abs(current_price - stop_loss) / current_price > risk_pct:
+            print("Risk limit exceeded. Order not placed.")
+            return None
         order = client.create_order(
             symbol=symbol,
             side=side,
-            type="LIMIT",
-            timeInForce="GTC",
+            type=Client.ORDER_TYPE_LIMIT,
+            timeInForce=Client.TIME_IN_FORCE_GTC,
             quantity=quantity,
             price=current_price,
+            stopPrice=stop_loss,
+            stopLimitPrice=take_profit,
+            stopLimitTimeInForce=Client.TIME_IN_FORCE_GTC,
         )
         print(
             f"{side.capitalize()} order of {quantity} {symbol} at {current_price:.2f} placed."
@@ -83,29 +87,84 @@ def place_order(side, quantity, symbol, price=None):
         return None
 
 
+def plot_chart(
+    close_prices, short_ma, long_ma, upper_band, lower_band, buy_signals, sell_signals
+):
+    data = pd.DataFrame()
+    data["Close"] = close_prices
+    data["Short_MA"] = short_ma
+    data["Long_MA"] = long_ma
+    data["Upper_Band"] = upper_band
+    data["Lower_Band"] = lower_band
+    data["Buy_Signal"] = buy_signals
+    data["Sell_Signal"] = sell_signals
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(data["Close"], label="Close", color="black", alpha=0.3)
+    plt.plot(data["Short_MA"], label="Short MA", color="blue", alpha=0.7)
+    plt.plot(data["Long_MA"], label="Long MA", color="red", alpha=0.7)
+    plt.plot(data["Upper_Band"], label="Upper Band", color="green", alpha=0.7)
+    plt.plot(data["Lower_Band"], label="Lower Band", color="orange", alpha=0.7)
+    plt.scatter(
+        data.index,
+        data["Buy_Signal"],
+        label="Buy Signal",
+        marker="^",
+        color="green",
+        alpha=1,
+    )
+    plt.scatter(
+        data.index,
+        data["Sell_Signal"],
+        label="Sell Signal",
+        marker="v",
+        color="red",
+        alpha=1,
+    )
+    plt.title("Price Chart with Moving Averages and Bollinger Bands")
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.legend(loc="upper left")
+    plt.show()
+
+
 def main():
+    close_prices, current_price = get_price_data()
+    buy_signals = [None] * len(close_prices)
+    sell_signals = [None] * len(close_prices)
+
     while True:
-        # 価格データを取得する
         close_prices, current_price = get_price_data()
-
-        # 移動平均線を計算する
+        print(f"Current price: {current_price:.2f}")
         short_ma, long_ma = calc_moving_average(close_prices)
-        print(f"現在価格：{current_price}")
-        print(f"短期移動平均線：{short_ma}")
-        print(f"長期移動平均線：{long_ma}")
+        print(f"Short MA: {short_ma:.2f}")
+        sma, upper_band, lower_band = calc_bollinger_bands(close_prices)
+        print(f"Upper band: {upper_band:.2f}")
 
-        # 売買条件を判断する
-        if short_ma > long_ma:
-            # 買い注文を出す
-            order = place_order("buy", quantity, "BTCUSDT")
-            print(f"買い注文を出しました：{order}")
-        elif short_ma < long_ma:
-            # 売り注文を出す
-            order = place_order("sell", quantity, "BTCUSDT")
-            print(f"売り注文を出しました：{order}")
-
-        # 一定時間待機する
-        time.sleep(60)
+        if short_ma > long_ma and current_price > lower_band:
+            print("Buy signal")
+            order = place_order("buy", quantity, symbol, stop_loss_pct, take_profit_pct)
+            print(order)
+            buy_signals[-1] = current_price
+            print(buy_signals[-1])
+        elif short_ma < long_ma and current_price < upper_band:
+            print("Sell signal")
+            order = place_order(
+                "sell", quantity, symbol, stop_loss_pct, take_profit_pct
+            )
+            print(order)
+            sell_signals[-1] = current_price
+            print(sell_signals[-1])
+        plot_chart(
+            close_prices,
+            short_ma,
+            long_ma,
+            upper_band,
+            lower_band,
+            buy_signals,
+            sell_signals,
+        )
+        time.sleep(5)
 
 
 if __name__ == "__main__":
